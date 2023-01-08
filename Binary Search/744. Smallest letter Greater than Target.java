ceada962adb4caa7cead38fe274be337858ad56d
Astush Sharma <86668736+ashu-0453@users.noreply.github.com>
2023-01-09 01:05:20 +0530

    Create 744. Smallest letter Greater than Target.java

---
+++ Binary Search/744. Smallest letter Greater than Target.java
@@ -0,0 +1,15 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int s=0,e=letters.length-1,mid;
+        while(s<=e){
+            mid=e-(e-s)/2;
+            if(target<letters[mid]){
+               e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+        }
+        return letters[s%letters.length];
+    }
+}
