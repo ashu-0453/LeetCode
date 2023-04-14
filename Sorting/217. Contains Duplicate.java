6d78e44b0cf60d7c05e9c192c993db5a19badc22
Astush Sharma <86668736+ashu-0453@users.noreply.github.com>
2023-04-14 16:43:18 +0530

    LeetCode Easy

---
+++ Sorting/217. Contains Duplicate.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j>0;j--){
+                if(nums[j]==nums[j-1]){
+                    return true;
+                }
+                else if(nums[j]<nums[j-1]){
+                    int temp=nums[j];
+                    nums[j]=nums[j-1];
+                    nums[j-1]=temp;
+                }else{
+                    break;
+                }
+            }
+        }
+         return false;   
+    }
+}
