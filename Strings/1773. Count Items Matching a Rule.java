eb91868d3093f4da345c056973fbb952c7969f5a
Astush Sharma <86668736+ashu-0453@users.noreply.github.com>
2023-05-04 01:44:57 +0530

    LeetCode Easy

---
+++ Strings/1773. Count Items Matching a Rule.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int count=0;
+        switch(ruleKey){
+            case "type":
+                for(int i=0;i<items.size();i++){
+                    if(items.get(i).get(0).equals(ruleValue)){
+                        count++;
+                    }
+                }
+                break;
+
+            case "color":
+                for(int i=0;i<items.size();i++){
+                    if(items.get(i).get(1).equals(ruleValue)){
+                        count++;
+                    }
+                }
+                break;
+
+            case "name":
+                for(int i=0;i<items.size();i++){
+                    if(items.get(i).get(2).equals(ruleValue)){
+                        count++;
+                    }
+                }
+                break;            
+        }
+    return count;
+        
+    }
+}
