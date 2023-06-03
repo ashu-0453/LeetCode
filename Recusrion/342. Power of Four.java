a181ef51c31ed469e27e035f4f570c4024361a98
Astush Sharma <86668736+ashu-0453@users.noreply.github.com>
2023-06-04 03:21:18 +0530

    LeetCode Easy

---
+++ Recusrion/342. Power of Four.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==1){
+            return true;
+        }
+        if(n%4!=0 || n<=0){
+            return false;
+        }
+        
+        n=n/4;
+        return isPowerOfFour(n);
+        
+    }
+}
