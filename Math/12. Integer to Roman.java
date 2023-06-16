064a6bca0ceb6824183a81123e2164f2defb9ce1
Astush Sharma <86668736+ashu-0453@users.noreply.github.com>
2023-06-17 03:04:18 +0530

    LeetCode Medium

---
+++ Math/12. Integer to Roman.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String intToRoman(int num) {
+        int[] val={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] roman={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder ans=new StringBuilder();
+        for(int i=0;num > 0;i++){
+            while(num >= val[i]){
+                ans.append(roman[i]);
+                num=num-val[i];
+            }
+        }
+        return ans.toString();
+    }
+}
