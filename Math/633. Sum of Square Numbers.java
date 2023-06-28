563eaf1fc4cf846c18c0ac1429910028c531e031
Astush Sharma <86668736+ashu-0453@users.noreply.github.com>
2023-06-28 23:45:58 +0530

    LeetCode Medium

---
+++ Math/633. Sum of Square Numbers.java
@@ -0,0 +1,15 @@
+//Brute Force Approach 
+//Best Approach : Fermat Theorem
+
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        for(long a=0;a*a <= c;a++){
+            for(long b=0;b*b <= c;b++){
+                if((a*a + b*b) ==c){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
