26be4b62317138d249831115093892239c05659c
Astush Sharma <86668736+ashu-0453@users.noreply.github.com>
2023-06-28 23:43:57 +0530

    LeetCode Medium

---
+++ Binary Search/633. Sum of Square Numbers.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        for(long a=0;a*a <= c;a++){
+            int b=c-(int)(a*a);
+            if(binarySearch(0,b,b)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean binarySearch(long s,long e,int t){
+        if(s>e){
+            return false;
+        }
+        long mid=s+(e-s)/2;
+        if(mid*mid == t){
+            return true;
+        }
+        if(mid*mid > t){
+            return binarySearch(s,mid-1,t);
+        }
+
+        return binarySearch(mid+1,e,t);
+
+    }
+}
